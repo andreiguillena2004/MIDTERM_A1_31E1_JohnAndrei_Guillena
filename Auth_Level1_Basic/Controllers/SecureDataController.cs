49c4a384276d8f4656399d2bc92c500d1baac3c2
Niño Francisco Alamo <Nio Francisco Alamo@ISTK>
2026-01-21 17:58:56 +0800

    Add project files.

---
+++ Auth_Level1_Basic/Controllers/SecureDataController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth_Level1_Basic.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SecureDataController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        // We know user exists because Middleware verified it
+        return Ok("You have accessed the Secure Data!");
+    }
+}
